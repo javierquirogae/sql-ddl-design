4047bd2360f0b16b785da040aff5e15d339cdfdd
Javier Quiroga <javier.quiroga.e@gmail.com>
2023-01-22 17:13:48 -0500

    done

--- air_traffic.sql
+++ air_traffic.sql
@@ -7,31 +7,148 @@ CREATE DATABASE air_traffic;
 
 \c air_traffic
 
+CREATE TABLE airports
+(
+	id      SERIAL PRIMARY KEY,
+	city    TEXT NOT NULL,
+	country TEXT NOT NULL
+);
+
+INSERT INTO airports
+  	(city, country)
+VALUES
+	('Washington DC', 	'United States'),
+	('Seattle', 		'United States'),
+	('Tokyo', 			'Japan'),
+	('London', 			'United Kingdom'),
+	('Los Angeles', 	'United States'),
+	('Las Vegas', 		'United States'),
+	('Mexico City', 	'Mexico'),
+	('Paris', 			'France'),
+	('Casablanca', 		'Morocco'),
+	('Dubai', 			'UAE'),
+	('Beijing', 		'China'),
+	('New York', 		'United States'),
+	('Charlotte', 		'United States'),
+	('Cedar Rapids', 	'United States'), 
+	('Chicago', 		'United States'),
+	('New Orleans', 	'United States'),
+	('Sao Paolo', 		'Brazil'), 
+	('Santiago', 		'Chile');
+
+CREATE TABLE airlines
+(
+	id 		SERIAL PRIMARY KEY,
+	airline 	TEXT NOT NULL
+);
+
+INSERT INTO airlines
+  	(airline)
+VALUES
+	('United'),
+	('British Airways'),
+	('Delta'),
+	('TUI Fly Belgium'),
+	('Air China'),
+	('American Airlines'),
+	('Avianca Brasil');
+
+CREATE TABLE customers
+(
+	id 			SERIAL PRIMARY KEY,
+	first_name 	TEXT NOT NULL,
+	last_name 	TEXT NOT NULL
+);
+
+
+INSERT INTO customers
+ 	(first_name, last_name)
+VALUES
+	('Jennifer', 	'Finch'),
+	('Thadeus', 	'Gathercoal'),
+	('Sonja', 		'Pauley'),
+	('Jennifer', 	'Finch'),
+	('Waneta', 		'Skeleton'),
+	('Thadeus', 	'Gathercoal'),
+	('Berkie', 		'Wycliff'),
+	('Alvin', 		'Leathes'),
+	('Berkie', 		'Wycliff'),
+	('Cory', 		'Squibbes');
+
+
+CREATE TABLE routes
+(
+	id SERIAL PRIMARY KEY,
+	from_port int NOT NULL references airports(id),
+	to_port int NOT NULL references airports(id)
+);
+
+INSERT INTO routes
+  	(from_port, to_port)
+VALUES
+	(1, 	2),
+	(3, 	4),
+	(5, 	6),
+	(2, 	7),
+	(8, 	9),
+	(10, 	11),
+	(12, 	13),
+	(14, 	15),
+	(13, 	16),
+	(17, 	18);
+
+
+
+
+
+
+CREATE TABLE flights
+(
+	id SERIAL PRIMARY KEY,
+	departure TIMESTAMP NOT NULL,
+	arrival TIMESTAMP NOT NULL,
+	airline int NOT NULL references airlines(id),
+	flight_route int NOT NULL references routes(id),
+	aircraft text NOT NULL
+);
+
+INSERT INTO flights
+  	(departure, arrival, airline, flight_route, aircraft)
+VALUES
+	('2018-04-08 09:00:00', '2018-04-08 12:00:00', 1, 1, 	'727'),
+	('2018-12-19 12:45:00', '2018-12-19 16:15:00', 2, 2, 	'727'),
+	('2018-01-02 07:00:00', '2018-01-02 08:03:00', 3, 3, 	'727'),
+	('2018-04-15 16:50:00', '2018-04-15 21:00:00', 3, 4, 	'747'),
+	('2018-08-01 18:30:00', '2018-08-01 21:50:00', 4, 5, 	'727'),
+	('2018-10-31 01:15:00', '2018-10-31 12:55:00', 5, 6, 	'727'),
+	('2019-02-06 06:00:00', '2019-02-06 07:47:00', 1, 7, 	'727'),
+	('2018-12-22 14:42:00', '2018-12-22 15:56:00', 6, 8, 	'727'),
+	('2019-02-06 16:28:00', '2019-02-06 19:18:00', 6, 9, 	'747'),
+	('2019-01-20 19:30:00', '2019-01-20 22:45:00', 7, 10, 	'777');
+
+
+
+
+
 CREATE TABLE tickets
 (
-  id SERIAL PRIMARY KEY,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  seat TEXT NOT NULL,
-  departure TIMESTAMP NOT NULL,
-  arrival TIMESTAMP NOT NULL,
-  airline TEXT NOT NULL,
-  from_city TEXT NOT NULL,
-  from_country TEXT NOT NULL,
-  to_city TEXT NOT NULL,
-  to_country TEXT NOT NULL
+	id SERIAL PRIMARY KEY,
+	customer int  NOT NULL references customers(id),
+	seat TEXT NOT NULL,
+	flight int NOT NULL references flights(id),
+	price float NOT NULL
 );
 
 INSERT INTO tickets
-  (first_name, last_name, seat, departure, arrival, airline, from_city, from_country, to_city, to_country)
+  	(customer, seat, flight, price)
 VALUES
-  ('Jennifer', 'Finch', '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United', 'Washington DC', 'United States', 'Seattle', 'United States'),
-  ('Thadeus', 'Gathercoal', '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 'British Airways', 'Tokyo', 'Japan', 'London', 'United Kingdom'),
-  ('Sonja', 'Pauley', '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 'Delta', 'Los Angeles', 'United States', 'Las Vegas', 'United States'),
-  ('Jennifer', 'Finch', '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 'Delta', 'Seattle', 'United States', 'Mexico City', 'Mexico'),
-  ('Waneta', 'Skeleton', '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 'TUI Fly Belgium', 'Paris', 'France', 'Casablanca', 'Morocco'),
-  ('Thadeus', 'Gathercoal', '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 'Air China', 'Dubai', 'UAE', 'Beijing', 'China'),
-  ('Berkie', 'Wycliff', '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 'United', 'New York', 'United States', 'Charlotte', 'United States'),
-  ('Alvin', 'Leathes', '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 'American Airlines', 'Cedar Rapids', 'United States', 'Chicago', 'United States'),
-  ('Berkie', 'Wycliff', '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 'American Airlines', 'Charlotte', 'United States', 'New Orleans', 'United States'),
-  ('Cory', 'Squibbes', '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 'Avianca Brasil', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile');
+	(1, 	'33B', 	1, 	'900'),
+	(2, 	'8A', 	2, 	'850'),
+	(3, 	'12F', 	3, 	'500'),
+	(4, 	'20A', 	4, 	'500'),
+	(5, 	'23D', 	5, 	'300'),
+	(6, 	'18C', 	6, 	'500'),
+	(7, 	'9E', 	7, 	'500'),
+	(8, 	'1A', 	8, 	'400'),
+	(9, 	'32B', 	9, 	'750'),
+	(10, 	'10D', 	10, '500');
--- music.sql
+++ music.sql
@@ -7,27 +7,156 @@ CREATE DATABASE music;
 
 \c music
 
+
+CREATE TABLE producers
+(
+	id SERIAL PRIMARY KEY,
+	producer TEXT NOT NULL
+);
+
+INSERT INTO producers
+ 	(producer)
+VALUES
+	('Dust Brothers'),
+	('Stephen Lironi'),
+	('Roy Thomas Baker'),
+	('Walter Afanasieff'),
+	('Benjamin Rice'),
+	('Rick Parashar'),
+	('Al Shux'),
+	('Max Martin'),
+	('Cirkut'),
+	('Shellback'),
+	('Benny Blanco'),
+	('The Matrix'),
+	('Darkchild');
+
+CREATE TABLE artists
+(
+	id SERIAL PRIMARY KEY,
+	artist TEXT NOT NULL
+);
+
+INSERT INTO artists
+  	(artist)
+VALUES
+	('Hanson'),
+	('Queen'),
+	('Mariah Cary'),
+	('Boyz II Men'),
+	('Lady Gaga'),
+	('Bradley Cooper'),
+	('Nickelback'),
+	('Jay Z'),
+	('Alicia Keys'),
+	('Katy Perry'),
+	('Juicy J'),
+	('Maroon 5'),
+	('Christina Aguilera'),
+	('Avril Lavigne'),
+	('Destiny''s Child');
+
+
+CREATE TABLE albums
+(
+	id SERIAL PRIMARY KEY,
+	album TEXT NOT NULL,
+	release_date DATE NOT NULL
+);
+
+INSERT INTO albums
+  	(release_date, album)
+VALUES
+	('04-15-1997', 'Middle of Nowhere'),
+	('10-31-1975', 'A Night at the Opera'),
+	('11-14-1995', 'Daydream'),
+	('09-27-2018', 'A Star Is Born'),
+	('08-21-2001', 'Silver Side Up'),
+	('10-20-2009', 'The Blueprint 3'),
+	('12-17-2013', 'Prism'),
+	('06-21-2011', 'Hands All Over'),
+	('05-14-2002', 'Let Go'),
+	('11-07-1999', 'The Writing''s on the Wall');
+
+
+
+
+
+
+
 CREATE TABLE songs
 (
-  id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  duration_in_seconds INTEGER NOT NULL,
-  release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+	id SERIAL PRIMARY KEY,
+	title TEXT NOT NULL,
+	duration_in_seconds INTEGER NOT NULL,
+	album int NOT NULL references albums(id)
 );
 
 INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+  	(title, duration_in_seconds, album)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+	('MMMBop', 					238, 1),
+	('Bohemian Rhapsody', 		355, 2),
+	('One Sweet Day', 			282, 3),
+	('Shallow', 				216, 4),
+	('How You Remind Me', 		223, 5),
+	('New York State of Mind', 	276, 6),
+	('Dark Horse', 				215, 7),
+	('Moves Like Jagger', 		201, 8),
+	('Complicated', 			244, 9),
+	('Say My Name', 			240, 10);
+
+
+create table producers_albums(
+	id SERIAL PRIMARY KEY,
+	producer int references producers(id),
+	album int references albums(id)
+
+);
+
+
+INSERT INTO producers_albums
+  	(producer, album)
+VALUES
+	(1, 1),
+	(2, 1),
+	(3, 2),
+	(4, 3),
+	(5, 4),
+	(6, 5),
+	(7, 6),
+	(8, 7),
+	(9, 7),
+	(10, 8),
+	(11, 8),
+	(12, 9),
+	(13, 10);
+
+
+
+create table artists_songs(
+	id SERIAL PRIMARY KEY,
+	artist int references artists(id),
+	song int references songs(id)
+
+);
+
+
+INSERT INTO artists_songs
+  	(artist, song)
+VALUES
+	(1, 	1),
+	(2, 	2),
+	(3, 	3),
+	(4, 	3),
+	(5, 	4),
+	(6, 	4),
+	(7, 	5),
+	(8, 	6),
+	(9, 	6),
+	(10, 	7),
+	(11, 	7),
+	(12, 	8),
+	(13, 	8),
+	(14, 	9),
+	(15, 	10);
